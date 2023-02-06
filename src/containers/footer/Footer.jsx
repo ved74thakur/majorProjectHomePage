4abaae4b92cd3d68d12f61653f5854a7c487765e
ved74thakur <ved74thakur@gmail.com>
2023-02-06 19:55:04 +0530

    first commit

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -15,11 +15,12 @@ const Footer = () => (
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+        <p>
+          B9 Major Project <br /> All Rights Reserved
+        </p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Links</h4>
-        <p>Overons</p>
         <p>Social Media</p>
         <p>Counters</p>
         <p>Contact</p>
@@ -32,14 +33,14 @@ const Footer = () => (
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+        <p>Terna Engineering College</p>
         <p>085-132567</p>
         <p>info@payme.net</p>
       </div>
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
+      <p>@2023 GrowthCap. All rights reserved.</p>
     </div>
   </div>
 );
